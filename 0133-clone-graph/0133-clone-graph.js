3aee3821cda2276751b126a756462b5e3d989341
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-03-08 18:50:13 -0500

    Time: 59 ms (91.61%), Space: 43.9 MB (55.40%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.js
@@ -0,0 +1,48 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+var traverse = function(node, map) {
+    // while crawl through graph
+    // construct new nodes along the way
+    // somehow reach every node
+        // map new nodes
+            // key is index: val is reference
+    
+    if (!map[node.val]) {
+        map[node.val] = new Node(node.val)
+    } else {
+        return map[node.val]    // if it exists, we just return it
+    }
+
+    node.neighbors.map(neighbor => {
+        // make sure new node, i.e. map[node.val]
+        // has cloned versions of these in its neighbors
+        return map[node.val].neighbors.push(traverse(neighbor, map))
+    })
+
+    return map[node.val]
+
+    // return clone of node you passed it
+    // and ensure all connections exist
+}
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+
+var cloneGraph = function(node) {
+    if (!node) return null
+
+    const map = {}
+    
+    // running map of visited nodes
+    // for every node in neighbors list for a node
+        // if we visited, don't descend recursively, index into map
+    return traverse(node, map)
+};
