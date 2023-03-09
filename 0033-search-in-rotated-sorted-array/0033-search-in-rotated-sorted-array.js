e1bc6fc488f417916de7a858487e16ea1c84ae03
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-03-09 13:35:35 -0500

    Time: 74 ms (19.67%), Space: 41.9 MB (64.71%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+/*
+O(n)
+    look @ first ele
+    look @ last ele
+    if last < first - rotate by one til it's true
+    return number of times you rotated
+    shift() + push()
+
+O(logn) - binary search?
+    find the smallest value, idx
+    
+
+*/
+
+const search = (nums, target) => {
+    // binary search nums arr if sorted
+    const smallestIdx = nums.findIndex((num, idx) => idx>0 && nums[idx-1] > num)
+    
+    let left = 0
+    let right = 0
+    
+    if (target > nums[nums.length-1]) {
+        // look to left of target idx
+        left = 0
+        right = smallestIdx-1
+    } else {
+        // look to right of target idx
+        left = smallestIdx
+        right = nums.length-1
+    }
+    
+    while (left <= right) {
+        let mid = Math.floor((left+right) / 2)
+        
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
+            right = mid - 1 // we looked @ mid already
+       } else {
+            left = mid + 1 // we looked @ mid already dont forget zack!
+       }
+    }
+    
+    return -1
+};
