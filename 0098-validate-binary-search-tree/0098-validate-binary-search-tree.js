8cd4c0b8a26b18b760a018f3228fe331ce98145f
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-04-24 12:39:47 -0400

    Time: 79 ms (23.43%), Space: 46 MB (89.09%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+
+
+const isValidBST = (root) => {
+    // start with root
+    // root.left.val < root.val < root.right.val
+    // left subtree contains nodes less than the node's key
+    // right subtree contains nodes greater than node's key
+    
+    // left + right subtrees must be BST
+    
+    // start at root's children
+    if (isValidNode(root.left, -Infinity, root.val) === false) {
+        return false
+    }
+    
+    if (isValidNode(root.right, root.val, Infinity) === false) {
+        return false
+    }
+    
+    // initially
+    // if we go left, we need to bound right with maxSeen
+    // if we go right, we need to bound left with minSeen
+    
+    return true
+}
+
+// this is where the recursion happens
+const isValidNode = (node, minSeen, maxSeen) => {
+    // if there's nothing on this side, it's valid
+    if (!node) return true
+
+    if ((minSeen < node.val && node.val < maxSeen) === false) {
+        return false
+    }
+    
+    if (isValidNode(node.left, minSeen, node.val) === false) {
+        return false
+    }
+
+    if (isValidNode(node.right, node.val, maxSeen) === false) {
+        return false
+    }
+    
+    return true
+}
