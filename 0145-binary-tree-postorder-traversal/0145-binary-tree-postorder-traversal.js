49a8126089f0cb292b0c0de0ef5467d41e06e004
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-05-01 15:45:09 -0400

    Time: 62 ms (37.42%), Space: 41.6 MB (91.56%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.js
@@ -11,23 +11,43 @@
  * @return {number[]}
  */
 
-// 
 var postorderTraversal = (root) => {
-    // once children are null
-    // add that node's val to arr
-    // base case
+    if (!root) return []
     
     const res = []
-    traverse(root, res)
+    const stack = []
+    const visited = new Set()
+    let curr = root
+    stack.push(curr)
+    
+    while (stack.length) {
+        curr = stack[stack.length - 1]
+        if (curr.left && !visited.has(curr.left)) {
+            stack.push(curr.left)
+            continue
+        }
+        if (curr.right && !visited.has(curr.right)) {
+            stack.push(curr.right)
+            continue
+        }
+        
+        stack.pop()
+        res.push(curr.val)
+        visited.add(curr)
+    }
     
     return res
+    
+//     const res = []
+//     traverse(root, res)
+//     return res
 }
 
-const traverse = (curr, arr) => {
-    if (!curr) return
-    else {
-        if (curr.left) traverse(curr.left, arr)
-        if (curr.right) traverse(curr.right, arr)
-        arr.push(curr.val)
-    }
-}
+// const traverse = (curr, arr) => {
+//     if (!curr) return
+//     else {
+//         if (curr.left) traverse(curr.left, arr)
+//         if (curr.right) traverse(curr.right, arr)
+//         arr.push(curr.val)
+//     }
+// }
