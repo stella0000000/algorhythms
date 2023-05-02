d2873142d9884714d1b151b7be445b4b83ce9e52
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-05-02 11:17:45 -0400

    Time: 64 ms (93.11%), Space: 48.1 MB (75.56%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.js
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.js
@@ -12,14 +12,23 @@
  * @return {number}
  */
 
-// input: root of BST, int k
+// input: root of BST, int k 
 // return kth smallest 1-index value of nodes in the tree
 
-var kthSmallest = function(root, k) {
-    // pre-order traversal
-    // we can obtain the node values sorted
-    // initialize array, push to array til we have length of k-1
+// initialize curr at root
+    // continue traversing to leftest leaf
+
+const kthSmallest = (root, k) => {
+    // inorder traversal
+        // get the leftmost node
+        // go back up
+        // traverse to right node
+        // down to right leaf
+        // think: zig zag
+    
+    if (!root) return null
     let count = 0
+    // const arr = []
     const stack = []
     let curr = root
     
@@ -29,10 +38,49 @@ var kthSmallest = function(root, k) {
             curr = curr.left
         }
         
+        // now we're at leftmost leaf
+        
         curr = stack.pop()
-        count ++
+        count++
         if (count === k) return curr.val
+        // arr.push(curr.val)
         curr = curr.right
     }
     
-};
+    // console.log(arr)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var kthSmallest = function(root, k) {
+//     // pre-order traversal
+//     // we can obtain the node values sorted
+//     // initialize array, push to array til we have length of k-1
+//     let count = 0
+//     const stack = []
+//     let curr = root
+    
+//     while (curr || stack.length) {
+//         while (curr) {
+//             stack.push(curr)
+//             curr = curr.left
+//         }
+        
+//         curr = stack.pop()
+//         count ++
+//         if (count === k) return curr.val
+//         curr = curr.right
+//     }
+    
+// };
