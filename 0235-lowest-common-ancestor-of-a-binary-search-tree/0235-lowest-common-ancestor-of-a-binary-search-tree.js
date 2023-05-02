aaf6c494171685f4209f9ae709840c649b44126b
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-05-02 12:26:06 -0400

    Time: 145 ms (5.15%), Space: 61.8 MB (5.19%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+
+const lowestCommonAncestor = (root, p, q) => {
+    // find node j where p <= j <= q where p <= q
+    // if p = q -> j = p and q
+    let curr = root
+    
+    let smaller, larger
+    if (p.val < q.val) {
+        smaller = p.val
+        larger = q.val
+    } else {
+        smaller = q.val
+        larger = p.val
+    }
+    
+    while (curr) {
+        console.log({ curr, smaller, larger })
+        if (smaller <= curr.val && curr.val <= larger) {
+            return curr
+        } else if (smaller < curr.val && larger < curr.val) {
+            curr = curr.left
+        } else {
+            curr = curr.right
+        }
+    }
+}
