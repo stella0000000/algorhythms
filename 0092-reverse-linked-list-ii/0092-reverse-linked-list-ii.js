4661d9694cc8fb14166688de87e7b2dbc5de76c3
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-05-04 18:07:19 -0400

    Time: 55 ms (75.75%), Space: 41.9 MB (82.12%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
@@ -12,45 +12,145 @@
  * @return {ListNode}
  */
 
-var reverseBetween = function(head, m, n) {
-  var dummyNode = new ListNode(0);
-  var now = dummyNode;
-  var tmp = null;
-  var reverseLast = null;
-  var reverseHead = null;
-  var reverseNow = null;
-  var i = 0;
-
-  dummyNode.next = head;
-
-  while (now) {
-    tmp = now.next;
-
-    if (i === m - 1) {
-      reverseHead = now;
+const reverseBetween = (head, left, right) => {
+    let dummyNode = new ListNode(0, head)
+    
+    // reach node at position "left"
+    let leftPrev = dummyNode
+    let curr = head
+    
+    for (let i=0; i<left-1; i++) {
+        leftPrev = curr
+        curr = curr.next
     }
-
-    if (i === m) {
-      reverseLast = now;
-      reverseNow = now;
+    
+    // now curr="left", leftPrev = node before left
+    // reverse from left to right
+    let prev = null
+    for (let i=0; i<right-left+1; i++) {
+        let tempNext = curr.next
+        curr.next = prev
+        prev = curr
+        curr = tempNext
     }
-
-    if (i > m && i <= n) {
-      now.next = reverseNow;
-      reverseNow = now;
-    }
-
-    if (i === n) {
-      reverseLast.next = tmp;
-      reverseHead.next = reverseNow;
-    }
-
-    now = tmp;
-    i++;
-  }
-
-  return dummyNode.next;
+    
+    leftPrev.next.next = curr
+    leftPrev.next = prev
+    return dummyNode.next
+    
 }
+
+// const reverseBetween = (head, left, right) => {
+//     if (!head.next || left === right) return head
+    
+//     // we're just swapping these two nodes
+//     if (right-left === 1) {
+//         let dummyNode = new ListNode(head.next.val, head)
+//         dummyNode.next.next = null
+//         return dummyNode
+//     }
+//     // if left === 1 issue
+//     // if right === length of list issue
+    
+//     let curr = head
+//     let posOfBeforeLeft = left - 1
+    
+//     let tail = head
+//     let posOfAfterRight = right + 1
+    
+//     while (posOfBeforeLeft > 1) {
+//         curr = curr.next
+//         posOfBeforeLeft--
+//     }
+    
+//     while (posOfAfterRight > 1) {
+//         tail = tail.next
+//         posOfAfterRight--
+//     }
+//     console.log({ curr, tail })
+
+//     // curr = node before left
+//     // tail = node after right
+    
+//     // NOW WE NEED TO ACQUIRE MIDDLE LL
+//     let count = 1
+//     let middleHead = head
+//     while (count !== left) {
+//         middleHead = middleHead.next
+//         count++
+//     }
+    
+//     let middleTail = middleHead
+//     while (middleTail && count < right) {
+//         middleTail = middleTail.next
+//         count++
+//     }
+    
+//     console.log({ middleHead, middleTail })
+//     middleTail.next = null
+    
+//     let reversed = reverse(middleHead)
+    
+//     curr.next = reversed
+//     middleHead.next = tail
+    
+//     // curr.next = middleReversed
+//     // lastNodeOfMiddleReversed.next = tail
+    
+//     return head
+// }
+
+// const reverse = (head) => {
+//     let prev = null
+//     let curr = head
+    
+//     while (curr) {
+//         let next = curr.next
+//         curr.next = prev
+//         prev = next
+//         prev = curr    // increment prev
+//         curr = next    // increment curr
+//     }
+    
+//     return prev
+// }
+
+// var reverseBetween = function(head, left, right) {
+//   var dummyNode = new ListNode(0)
+//   var curr = dummyNode
+//   var tmp = null
+//   var reverseLast = null
+//   var reverseHead = null
+//   var reverseCurr = null
+//   var i = 0
+
+//   dummyNode.next = head
+
+//   while (curr) {
+//     tmp = curr.next
+
+//     if (i === left - 1) {   // start of reversal
+//       reverseHead = curr
+//     } else if (i === left) {
+//       reverseLast = curr
+//       reverseCurr = curr
+//     } else if (i > left && i <= right) {
+//       curr.next = reverseCurr
+//       reverseCurr = curr
+//     }
+
+//     if (i === right) {      // we are now at head of the tail
+//       reverseLast.next = tmp
+//       reverseHead.next = reverseCurr
+//       break
+//     }
+
+//     curr = tmp
+//     i++
+//   }
+
+//   return dummyNode.next
+// }
 
 // const reverseBetween = (head, left, right) => {
 //     if (!head.next || left === right) return head
