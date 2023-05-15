4fced8879425d182c19abd97e485445f991bd409
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-05-15 13:19:37 -0400

    Time: 2235 ms (5.24%), Space: 150.5 MB (5.59%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.js
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.js
@@ -8,111 +8,111 @@
  */
 
 
-const findCheapestPrice = (n, flights, src, dst, K) => {    
-    // 1. Build Graph
-    const graph = buildGraph(flights, dst)
-    if (graph === null) return -1
+// const findCheapestPrice = (n, flights, src, dst, K) => {    
+//     // 1. Build Graph
+//     const graph = buildGraph(flights, dst)
+//     if (graph === null) return -1
     
-    // 2. BFS but with optimizations (a) and (b)
-    let queue = [{ from: src, costSoFar: 0, stopsLeft: K + 1 }]; // (1)
-    let cheapest = Infinity
+//     // 2. BFS but with optimizations (a) and (b)
+//     let queue = [{ from: src, costSoFar: 0, stopsLeft: K + 1 }]; // (1)
+//     let cheapest = Infinity
     
-    while(queue.length) {
-        let { from, costSoFar, stopsLeft } = queue.shift();
+//     while(queue.length) {
+//         let { from, costSoFar, stopsLeft } = queue.shift();
         
-        if (!graph[from]) continue; // no flights from this stop
+//         if (!graph[from]) continue; // no flights from this stop
         
-        // optimization (a) - simply don't continue searching this path if we've exhausted K stops
-        if(stopsLeft <= 0) continue
-        // optimization (b) - simply don't continue searching if we've already found a cheaper cost already
-        if (cheapest !== Infinity && costSoFar > cheapest) continue
+//         // optimization (a) - simply don't continue searching this path if we've exhausted K stops
+//         if(stopsLeft <= 0) continue
+//         // optimization (b) - simply don't continue searching if we've already found a cheaper cost already
+//         if (cheapest !== Infinity && costSoFar > cheapest) continue
         
-        for (let [nextStop, cost] of Object.entries(graph[from])) {
-            if(nextStop == dst) {
-                cheapest = Math.min(cheapest, costSoFar + cost)
-            } else {
-                queue.push({ from: nextStop, costSoFar: costSoFar + cost, stopsLeft: stopsLeft - 1 });
-            }
-        }
-    }
+//         for (let [nextStop, cost] of Object.entries(graph[from])) {
+//             if(nextStop == dst) {
+//                 cheapest = Math.min(cheapest, costSoFar + cost)
+//             } else {
+//                 queue.push({ from: nextStop, costSoFar: costSoFar + cost, stopsLeft: stopsLeft - 1 });
+//             }
+//         }
+//     }
     
-    return cheapest === Infinity ? -1 : cheapest
-}
+//     return cheapest === Infinity ? -1 : cheapest
+// }
 
-const buildGraph = (flights, dst) => {
-    const graph = {};
+// const buildGraph = (flights, dst) => {
+//     const graph = {};
     
-    let hasPathToDest = false;
+//     let hasPathToDest = false;
     
-    for(const [from, to, cost] of flights) {
-        if(!(from in graph)) graph[from] = {};
-        graph[from] = { ...graph[from], [to]: cost };
+//     for(const [from, to, cost] of flights) {
+//         if(!(from in graph)) graph[from] = {};
+//         graph[from] = { ...graph[from], [to]: cost };
         
-        if(to === dst) hasPathToDest = true;
-    }
+//         if(to === dst) hasPathToDest = true;
+//     }
     
-    return hasPathToDest ? graph : null;
-}
+//     return hasPathToDest ? graph : null;
+// }
 
 // n cities connected by some number of flights
 // return cheapest price from src to dst with @ most k stops
 // else return -1
 
-// var findCheapestPrice = function(n, flights, src, dst, k) {
-//     const graph = buildGraph(flights, dst)
-//     if (!graph) return -1
-//     let cheapestRoute = Infinity
-//     const queue = []
+var findCheapestPrice = function(n, flights, src, dst, k) {
+    const graph = buildGraph(flights, dst)
+    if (!graph) return -1
+    let cheapestRoute = Infinity
+    const queue = []
     
-//     for (let neighbor in graph[src]) {
-//              queue.push([{ [neighbor]: graph[src][neighbor] },
-//                            [0],
-//                            [k+1]
-//                         ])
-//         }
+    for (let neighbor in graph[src]) {
+             queue.push([{ [neighbor]: graph[src][neighbor] },
+                           [0],
+                           [k+1]
+                        ])
+        }
     
-//     while (queue.length) {
-//         const obj = queue.shift()
-//         const nextCity = Object.keys(obj[0])[0]
-//         const ticketPrice = Object.values(obj[0])[0]
-//         let runningPrice = Number(obj[1])
-//         let flightsLeft = Number(obj[2])
+    while (queue.length) {
+        const obj = queue.shift()
+        const nextCity = Object.keys(obj[0])[0]
+        const ticketPrice = Object.values(obj[0])[0]
+        let runningPrice = Number(obj[1])
+        let flightsLeft = Number(obj[2])
         
-//         runningPrice += ticketPrice // increment price
-//         flightsLeft--        // decrement flights left
+        runningPrice += ticketPrice // increment price
+        flightsLeft--        // decrement flights left
         
-//         if (Number(nextCity) === Number(dst)) {
-//             if (runningPrice < cheapestRoute) cheapestRoute = runningPrice
-//         }
+        if (Number(nextCity) === Number(dst)) {
+            if (runningPrice < cheapestRoute) cheapestRoute = runningPrice
+        }
         
-//         if (cheapestRoute !== Infinity && runningPrice > cheapestRoute) continue
+        if (cheapestRoute !== Infinity && runningPrice > cheapestRoute) continue
         
-//         if (flightsLeft === 0) continue
+        if (flightsLeft === 0) continue
 
-//         for (let neighbor in graph[nextCity]) {
-//              queue.push([{ [neighbor]: graph[nextCity][neighbor] },
-//                            [runningPrice],
-//                            [flightsLeft]
-//                         ])
-//         }
+        for (let neighbor in graph[nextCity]) {
+             queue.push([{ [neighbor]: graph[nextCity][neighbor] },
+                           [runningPrice],
+                           [flightsLeft]
+                        ])
+        }
         
-//     }
+    }
     
-//     return cheapestRoute !== Infinity ? cheapestRoute : -1
-// }
+    return cheapestRoute !== Infinity ? cheapestRoute : -1
+}
 
-// const buildGraph = (flights, dst) => {
-//     const graph = {}
-//     let hasPathToDst = false
+const buildGraph = (flights, dst) => {
+    const graph = {}
+    let hasPathToDst = false
     
-//     for (let flight of flights) {
-//         const [cityA, cityB, price] = flight
-//         // city0: { city1: 100, city2: 500 }
+    for (let flight of flights) {
+        const [cityA, cityB, price] = flight
+        // city0: { city1: 100, city2: 500 }
         
-//         if (!(cityA in graph)) graph[cityA] = {}
-//         if (!(cityB in graph[cityA])) graph[cityA][cityB] = price
+        if (!(cityA in graph)) graph[cityA] = {}
+        if (!(cityB in graph[cityA])) graph[cityA][cityB] = price
         
-//         if (Number(cityB) === Number(dst)) hasPathToDst = true
-//     }
-//     return hasPathToDst ? graph : null
-// }
+        if (Number(cityB) === Number(dst)) hasPathToDst = true
+    }
+    return hasPathToDst ? graph : null
+}
