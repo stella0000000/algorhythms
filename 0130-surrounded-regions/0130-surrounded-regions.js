207f864615c7453beec6e7fff71dcc006380ef71
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-05-26 13:50:09 -0400

    Time: 76 ms (90.82%), Space: 47.5 MB (40.20%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.js
@@ -0,0 +1,45 @@
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+
+// don't flip any 'O' group anchored at an edge
+
+var solve = function(board) {
+    const numRows = board.length
+    const numCols = board[0].length
+    
+    for (let r=0; r<numRows; r++) flip(board, r, 0)
+    for (let c=0; c<numCols; c++) flip(board, 0, c)
+    for (let r=0; r<numRows; r++) flip(board, r, numCols-1)
+    for (let c=0; c<numCols; c++) flip(board, numRows-1, c)
+    
+    for (let r=0; r<numRows; r++) {
+        for (let c=0; c<numCols; c++) {
+            if (board[r][c] === '#') board[r][c] = 'O'
+            else if (board[r][c] === 'O') board[r][c] = 'X'
+        }
+    }
+    
+    return board
+}
+
+const flip = (board, r, c) => {
+    const rInBounds = r >= 0 && r < board.length
+    const cInBounds = c >= 0 && c < board[0].length
+    
+    if (!rInBounds || !cInBounds) return
+    if (board[r][c] === 'X' || board[r][c] === '#') return
+    
+    board[r][c] = '#'
+
+    const deltas = [ [1,0], [-1,0], [0,1], [0,-1] ]
+    
+    for (let delta of deltas) {
+        const [dr, dc] = delta
+        const nr = r + dr
+        const nc = c + dc
+        
+        flip(board, nr, nc)
+    }
+}
