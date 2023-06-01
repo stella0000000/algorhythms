e3c2920ba18775cfc6fbf50a4027a59d536734f0
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-06-01 14:29:47 -0400

    Time: 58 ms (79.41%), Space: 44.1 MB (18.91%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+// level order traversal
+// 
+
+var rightSideView = function(root) {
+    const levels = {}
+    fillLevels(root, levels, 0)
+    
+    const res = []
+    
+    for (let level in levels) {
+        res.push(levels[level].pop())
+    }
+    
+    return res
+}
+
+const fillLevels = (root, levels, depth) => {
+    if (!root) return
+    
+    if (!(depth in levels)) {
+        levels[depth] = [root.val]
+    } else {
+        levels[depth].push(root.val)
+    }
+    
+    fillLevels(root.left, levels, depth+1)
+    fillLevels(root.right, levels, depth+1)
+}
