d0c11034470fa041b064046900b794a9429fbf16
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-06-06 13:02:17 -0400

    Time: 80 ms (18.77%), Space: 46.5 MB (53.23%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -12,48 +12,17 @@
  */
 
 const isValidBST = (root) => {
-    // start with root
-    // root.left.val < root.val < root.right.val
-    // left subtree contains nodes less than the node's key
-    // right subtree contains nodes greater than node's key
-    
-    // left + right subtrees must be BST
-    
-    // start at root's children
-    if (isValidNode(root.left, -Infinity, root.val) === false) {
-        return false
-    }
-    
-    if (isValidNode(root.right, root.val, Infinity) === false) {
-        return false
-    }
-    
-    // initially
-    // if we go left, we need to bound right with maxSeen
-    // if we go right, we need to bound left with minSeen
-    
-    return true
+    return isValidSubtree(root, -Infinity, Infinity)
 }
 
 // this is where the recursion happens
-const isValidNode = (node, minSeen, maxSeen) => {
-    // if there's nothing on this side, it's valid
-    if (!node) return true
-
-    // is the node valid
-    if ((minSeen < node.val && node.val < maxSeen) === false) {
-        return false
-    }
+const isValidSubtree = (root, minSeen, maxSeen) => {
+    if (!root) return true
     
-    // is the left subtree valid => recursively, check the node, and its subtrees
-    if (isValidNode(node.left, minSeen, node.val) === false) {
-        return false
-    }
-
-    // is the right subtree valid => recursively, check the node, and its subtrees
-    if (isValidNode(node.right, node.val, maxSeen) === false) {
-        return false
-    }
+    if (root.val >= maxSeen || root.val <= minSeen) return false
     
-    return true
+    const leftBranch = isValidSubtree(root.left, minSeen, root.val)
+    const rightBranch = isValidSubtree(root.right, root.val, maxSeen)
+    
+    return leftBranch && rightBranch
 }
