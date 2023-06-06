3f3084f7c58583257f5101b1a6e07d1d29040ce9
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-06-06 12:30:11 -0400

    Time: 77 ms (47.68%), Space: 47.4 MB (50.86%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -21,26 +21,22 @@
 const isBalanced = (root) => {
     // check difference of the two branches
     // if difference is more than 1, return false, otherwise continue
-    
-    let balanced = true
-    
     const dfs = (root) => {
         if (!root) return 0
-        if (!balanced) return -1
         
         let leftHeight = dfs(root.left)
         let rightHeight = dfs(root.right)
         
+        if (leftHeight === -1 || rightHeight === -1) return -1
+        
         let balanceFactor = Math.abs(leftHeight - rightHeight)
         
-        if (balanceFactor > 1) balanced = false
+        if (balanceFactor > 1) return -1
         
         let height = 1 + Math.max(leftHeight, rightHeight)
         
         return height
     }
     
-    dfs(root)
-    
-    return balanced
+    return dfs(root) !== -1
 }
