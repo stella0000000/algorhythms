54af67ebdaf4ac26c0a003ade1d0338d3394cd56
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-06-14 11:47:17 -0400

    Time: 89 ms (5.17%), Space: 46.3 MB (77.24%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -11,18 +11,52 @@
  * @return {boolean}
  */
 
+// BST:
+    // curr.left < curr < curr.right
+
 const isValidBST = (root) => {
-    return isValidSubtree(root, -Infinity, Infinity)
+    return traverse(root, -Infinity, Infinity)
 }
 
-// this is where the recursion happens
-const isValidSubtree = (root, minSeen, maxSeen) => {
-    if (!root) return true
+const traverse = (node, min, max) => {
+    if (!node) return true
+    if (node.val <= min) return false
+    if (node.val >= max) return false
     
-    if (root.val >= maxSeen || root.val <= minSeen) return false
+    const isLeftBST = traverse(node.left, min, node.val)
+    const isRightBST = traverse(node.right, node.val, max)
     
-    const leftBranch = isValidSubtree(root.left, minSeen, root.val)
-    const rightBranch = isValidSubtree(root.right, root.val, maxSeen)
-    
-    return leftBranch && rightBranch
+    return isLeftBST && isRightBST
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const isValidBST = (root) => {
+//     return isValidSubtree(root, -Infinity, Infinity)
+// }
+
+// // this is where the recursion happens
+// const isValidSubtree = (root, minSeen, maxSeen) => {
+//     if (!root) return true
+    
+//     if (root.val >= maxSeen || root.val <= minSeen) return false
+    
+//     const leftBranch = isValidSubtree(root.left, minSeen, root.val)
+//     const rightBranch = isValidSubtree(root.right, root.val, maxSeen)
+    
+//     return leftBranch && rightBranch
+// }
