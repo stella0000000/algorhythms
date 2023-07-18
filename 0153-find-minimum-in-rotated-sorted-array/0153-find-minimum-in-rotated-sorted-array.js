b69aef569c79fe95043849400cc236a5038fc5a8
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-07-18 17:15:11 -0400

    Time: 63 ms (37.26%), Space: 42.2 MB (29.29%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMin = (nums) => {
+    let [left, right] = [0, nums.length - 1];
+
+    while (left < right) {
+        const mid = (left + right) >> 1;
+        const guess = nums[mid];
+        const [leftNum, rightNum] = [nums[left], nums[right]];
+
+        const isTarget = leftNum < rightNum;
+        if (isTarget) return leftNum;
+
+        const isTargetGreater = leftNum <= guess;
+        if (isTargetGreater) left = mid + 1;
+
+        const isTargetLess = guess < leftNum;
+        if (isTargetLess) right = mid;
+    }
+
+    return nums[left];
+    
+//     if (nums.length === 1) return nums[0]
+//     if (nums[nums.length-1] > nums[0]) return nums[0]
+//     let minVal = nums[nums.length-1]
+//     let left = 0
+//     let right = nums.length-1
+//     let mid = Math.floor((nums.length-1) / 2)
+    
+//     while (left < right) {
+//         if (nums[mid-1] > nums[mid]) return nums[mid]
+        
+//         if (minVal <= nums[mid]) {
+//             // keep checking right side
+//             left = mid+1
+//         } else if (minVal > nums[mid]) {
+//             // keep checking left side
+//             minVal = nums[mid]
+//             right = mid
+//         }
+//     }
+}
+
+// need to find where left neighbor is > curr.val
