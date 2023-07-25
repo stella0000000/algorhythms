ce5c20d03627ecf62beec58e696bf4b8281528f3
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-07-25 11:22:24 -0400

    Time: 216 ms (11.08%), Space: 74.5 MB (20.70%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function(edges) {
+    // build graph
+    const edgeCount = {}
+    
+    for (let edge of edges) {
+        const [nodeA, nodeB] = edge
+        if (!(nodeA in edgeCount)) edgeCount[nodeA] = 0
+        if (!(nodeB in edgeCount)) edgeCount[nodeB] = 0
+        
+        edgeCount[nodeA]++
+        edgeCount[nodeB]++
+    }
+    
+    return Object.keys(edgeCount).reduce((a, b) => edgeCount[a] > edgeCount[b] ? a : b)
+};
+
+// center is node with most edges surrounding it
