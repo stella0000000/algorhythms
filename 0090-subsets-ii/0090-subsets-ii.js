ac5660174170f991c3d236ac0434d87bf1f8ba2e
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-04 12:32:30 -0400

    Time: 54 ms (96.09%), Space: 44.3 MB (88.40%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -3,52 +3,78 @@
  * @return {number[][]}
  */
 
-var subsetsWithDup = function(nums) {
-    nums.sort()
-    const res = []
-    backtrack(0, [], nums, res)
-    return res
-};
-
-function backtrack(index, subset, nums, res) {
-    if (index > nums.length) return
-    
-    // console.log("res: ",res)
-    // console.log("subset: ",subset)
-    
-    res.push([...subset])
-    
-    for (let i = index; i < nums.length; i++) {
-        if(i > index && nums[i] === nums[i - 1]) continue
-        
-        subset.push(nums[i])
-        backtrack(i+1, subset, nums, res)
-        
-        subset.pop()
-    }
-    
-    return res
-}
-
+/*
+                   []
+           /                 \
+           1                  []
+        /     \             /   \
+   [1, 2]     [1]        [2]      []
+     / \      /   \      /  \     /  \
+  [122] [12] [12]* [1]*  [22] [2] [2]* []*
+  
+  * skipping
+*/
 // var subsetsWithDup = function(nums) {
-//     const result = [];
-//     nums.sort();
+//     nums.sort()
+//     const res = []
+//     backtrack(0, [], nums, res)
+//     return res
+// };
+
+// function backtrack(index, subset, nums, res) {
+//     // reached end base case
+//     if (index > nums.length) return
     
-//     const dfs = (arr, i) => {
-//         if (i === nums.length) {
-//             result.push(arr);
-//             return;
-//         }
-//         // take the current number
-//         dfs([...arr, nums[i]], i+1);
-//         // while not at end of array and duplicate after current number
-//         while (i+1 < nums.length && nums[i] === nums[i + 1]) {
-//             i++;
-//         }
-//         // leave the current number
-//         dfs([...arr], i+1);
+//     res.push([...subset])
+    
+//     for (let i = index; i < nums.length; i++) {
+//         // if i is past index, or two adjacent values are equal
+//         if (i > index && nums[i] === nums[i - 1]) continue
+        
+//         // include
+//         let num = nums[i]
+//         subset.push(num)
+//         backtrack(i+1, subset, nums, res)
+        
+//         subset.pop()
 //     }
     
-//     dfs([], 0);
-//     return result;
-// };
+//     return res
+// }
+
+// 1, 2, 2
+// res = [ [1, 2, 2], [1, 2], [1], [2, 2], [2], [] ]
+
+
+var subsetsWithDup = function(nums) {
+    const result = []
+    nums.sort()
+    
+    const dfs = (subset, i) => {
+        // reached end base case
+        if (i === nums.length) {
+            result.push([...subset])
+            return
+        }
+        
+        // All subsets that include nums[i]
+        let num = nums[i]
+        subset.push(num)
+        dfs(subset, i+1)
+        
+        // All subsets that don't include nums[i]
+        subset.pop()
+        
+        // exclude all duplicates of that number
+        // just skip that branch
+        while (i+1 < nums.length && nums[i] === nums[i + 1]) {
+            i++
+        }
+        
+        // leave the current number
+        dfs(subset, i+1)
+    }
+    
+    dfs([], 0)
+    return result
+};
