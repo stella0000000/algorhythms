dff79f2ef27530f0473ef13667c1d8d4896d55e8
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-04 10:35:01 -0400

    Time: 55 ms (90.25%), Space: 43.3 MB (67.11%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -2,40 +2,105 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums) {
-    // include or exclude
-    // return dfs(nums, [[]])
+
+const subsets = (nums) => {
+    const res = []
+    const subset = []
     
-    return dfs(nums, [[]])
+    const backtrack = (i) => {
+        if (i >= nums.length) {
+            res.push([...subset])
+            return
+        }
+        
+        let num = nums[i]
+        
+        subset.push(num)
+        backtrack(i+1)
+        
+        subset.pop()
+        backtrack(i+1)
+    }
     
+    backtrack(0)
+    return res
+}
+
+
+// dfs recursion
+// base case nums.length === 0 return [[]]
+// char nums[0]
+// remainder nums.slice(1)
+
+// const subsets = (nums) => {
+//     const res = []
+//     const subset = []
     
-    // bottom up approach return empty away all the way at bottom
-    // build up coming back .. up
+//     const dfs = (i) => {
+//         if (i >= nums.length) {
+//             res.push([...subset])
+//             return
+//         }
+        
+//         const currNum = nums[i]
+        
+//         subset.push(currNum)
+//         dfs(i+1)
+        
+//         subset.pop()
+//         dfs(i+1)
+//     }
+    
+//     dfs(0)
+//     return res
+// }
+
+// const subsets = (nums) => {
 //     if (nums.length === 0) return [[]]
     
-//     let char = nums[0]
-//     let remainder = nums.slice(1)
+//     const char = nums[0]
+//     const remainder = nums.slice(1)
     
-//     let excludes = subsets(remainder)
-//     let includes = excludes.map(sub => [...sub, char])
+//     const exclude = subsets(remainder)
+//     const include = exclude.map(subset => [char, ...subset])
     
-//     return [...excludes, ...includes]
-};
+//     return [...include, ...exclude]
+// }
 
-const dfs = (nums, currSubsets) => {
-    // base case
-    if (nums.length === 0) return currSubsets
+// var subsets = function(nums) {
+//     // include or exclude
+//     // return dfs(nums, [[]])
+     
+//     return dfs(nums, [[]])
     
-    let remainder = nums.slice(1)
-    // let currCurrSubsets = currSubsets.map(x => [...x, nums[0]])
-    let exclude = dfs(remainder, currSubsets)
-    // 33 doesn't have anything to do with 31
-    currSubsets = currSubsets.map(x => [...x, nums[0]])
-    let include = dfs(remainder, currSubsets)
-    // let include = dfs(remainder, currCurrSubsets)
     
-    return [...include, ...exclude]
-}
+// //     // bottom up approach return empty away all the way at bottom
+// //     // build up coming back .. up
+// // //     if (nums.length === 0) return [[]]
+    
+// // //     let char = nums[0]
+// // //     let remainder = nums.slice(1)
+    
+// // //     let excludes = subsets(remainder)
+// // //     let includes = excludes.map(sub => [...sub, char])
+    
+// // //     return [...excludes, ...includes]
+// };
+
+// const dfs = (nums, currSubsets) => {
+//     // base case
+//     if (nums.length === 0) return currSubsets
+    
+//     let remainder = nums.slice(1)
+//     // let currCurrSubsets = currSubsets.map(subset => [...subset, nums[0]])
+//     let exclude = dfs(remainder, currSubsets)
+//     // exclude doesn't have anything to do with currCurrSubsets
+//     currSubsets = currSubsets.map(subset => [...subset, nums[0]])
+//     let include = dfs(remainder, currSubsets)
+//     // let include = dfs(remainder, currCurrSubsets)
+    
+//     return [...include, ...exclude]
+// }
 
 // return all possible subsets
 // no dupes, any order
