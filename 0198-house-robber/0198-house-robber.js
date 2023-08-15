2814854cc70c51453166f0fcad5e6bd80f683d7f
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-15 16:08:37 -0400

    Time: 44 ms (95.40%), Space: 41.6 MB (81.60%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,20 +3,52 @@
  * @return {number}
  */
 
+const rob = (nums) => {
+    const memo = new Array(nums.length).fill(0)
+    
+    memo[0] = nums[0]
+    memo[1] = Math.max(nums[0], nums[1])
+    
+    for (let i=2; i<nums.length; i++) {
+        memo[i] = Math.max(memo[i-2] + nums[i], memo[i-1])
+    }
+    
+    return memo[nums.length-1]
+}
+
+// var rob = function(nums) {
+//     const memo = {}
+//     return dfs(nums, memo, 0)
+// };
+
+// const dfs = (nums, memo, n) => {
+//     if (memo[n]) return memo[n]
+//     if (n >= nums.length) return 0
+    
+//     let robbed = nums[n] + dfs(nums, memo, n+2)
+//     let notRobbed = dfs(nums, memo, n+1)
+    
+//     memo[n] = Math.max(robbed, notRobbed)
+    
+//     return memo[n]
+// }
+
+
 // no 2 adjacent houses
 // nums[i] = amount of money in each house
 // return max amount of money without alerting police
+// var rob = function(nums, i=0) {    
+//     if (!nums.length) return 0
 
-var rob = function(nums) {    
-    if (nums.length === 0) return 0
-    if (nums.length === 1) return nums[0]
-    
-    // Keep track of max money we can make with x amount of houses available
-    let dp = [ nums[0], Math.max(nums[0], nums[1]) ]
+//     let [ left, mid ] = [ 0, 0 ]
 
-    for (let i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1])
-    }
-    
-    return dp[nums.length - 1]
-}
+//     for (const right of nums) { /* Time O(N) */
+//         const temp = mid
+//         const house = left + right
+
+//         mid = Math.max(mid, house)
+//         left = temp
+//     }
+
+//     return mid
+// }
