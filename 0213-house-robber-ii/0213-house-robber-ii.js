0823bbed2ea7e60bdf143d9c4326e32d486dc8c2
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-15 16:25:26 -0400

    Time: 40 ms (98.96%), Space: 42.1 MB (32.55%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.js
+++ 0213-house-robber-ii/0213-house-robber-ii.js
@@ -2,23 +2,33 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var rob = (nums) => {
     if (nums.length === 0) return 0
     if (nums.length === 1) return nums[0]
-    const withFirst = helper(nums.slice(0, nums.length-1))
-    console.log(nums.slice(0, nums.length-1))
-    const withoutFirst = helper(nums.slice(1))
-    return Math.max(withFirst, withoutFirst)
+
+    const left = search(nums, 0, (nums.length - 2))   /* Time O(N) */
+    const right = search(nums, 1, (nums.length - 1))  /* Time O(N) */
+
+    return Math.max(left, right)
 };
 
-const helper = (arr) => {
-    const memo = new Array(arr.length).fill(0)
-    memo[0] = arr[0]
-    memo[1] = Math.max(arr[0], arr[1])
-    
-    for (let i=2; i<arr.length; i++) {
-        memo[i] = Math.max(memo[i-2]+arr[i], memo[i-1])
+const search = (nums, start, end) => {
+    let [ left, mid ] = [ 0, 0 ]
+
+    for (let i = start; i <= end; i++) {  /* Time O(N) */
+        const temp = mid
+        const right = nums[i]
+        const house = left + right
+
+        mid = Math.max(mid, house)
+        left = temp
     }
-    
-    return memo[arr.length-1]
+
+    return mid
 }
