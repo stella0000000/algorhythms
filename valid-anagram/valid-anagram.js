ca4c6e0349cfba57e928dac75fbc4c75729b45ec
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-17 12:48:04 -0400

    Time: 65 ms, Memory: 44.4 MB - LeetHub

---
+++ valid-anagram/valid-anagram.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false
+    
+    const hash = {}
+    
+    for (let letter of s) {
+        if (!hash[letter]) hash[letter] = 0
+        hash[letter]++
+    }
+    
+    for (let letter of t) {
+        if (!hash[letter]) return false
+        hash[letter]--
+    }
+    
+    return Object.values(hash).every(v => v === 0)
+};
