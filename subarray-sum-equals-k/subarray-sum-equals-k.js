23d2c8fcbdc96ddbacac7ee899e3fcd63097fa58
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-21 15:38:05 -0400

    Time: 104 ms, Memory: 53.1 MB - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.js
+++ subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -3,18 +3,19 @@
  * @param {number} k
  * @return {number}
  */
+// 1 1 1 ==> 2
+
 
 var subarraySum = function(nums, k) {
+    // { prefixSum : count }
     const hashmap = { 0: 1 }
     let count = 0
     let sum = 0
     
     for (const num of nums) {
         sum += num
-        let complement = sum - k
-        console.log({ complement })
-        
-        if (hashmap[complement]) count += hashmap[complement]
+        let prefixSum = sum - k
+        if (hashmap[prefixSum]) count += hashmap[prefixSum]
         
         hashmap[sum] ? hashmap[sum]++ : hashmap[sum] = 1
     }
