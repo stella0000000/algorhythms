ac1248be798bd4bc639f61d17c8eaf2cec3e11ff
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-21 13:58:09 -0400

    Time: 51 ms, Memory: 41.3 MB - LeetHub

--- spiral-matrix/spiral-matrix.js
+++ spiral-matrix/spiral-matrix.js
@@ -6,6 +6,7 @@ var spiralOrder = function(matrix) {
     const numRows = matrix.length
     const numCols = matrix[0].length
     const total = numRows * numCols
+    
     const res = []
     
     let upperB = 0
@@ -17,25 +18,25 @@ var spiralOrder = function(matrix) {
         // rightward
         if (upperB <= lowerB) {
             for (let i = leftB; i <= rightB; i++) res.push(matrix[upperB][i])
-            upperB++;
+            upperB++ // took care of top row
         }
         
         // downward
         if (leftB <= rightB) {
             for (let i = upperB; i<= lowerB; i++) res.push(matrix[i][rightB])
-            rightB--
+            rightB-- // took care of last col
         }
         
         // leftward
         if (upperB <= lowerB) {
             for (let i = rightB; i >= leftB; i--) res.push(matrix[lowerB][i])
-            lowerB--
+            lowerB-- // took care of bottom row
         }
         
         // upward
         if (leftB <= rightB) {
             for (let i = lowerB; i >= upperB; i--) res.push(matrix[i][leftB])
-            leftB++
+            leftB++ // took care of first col
         }
     }
     
