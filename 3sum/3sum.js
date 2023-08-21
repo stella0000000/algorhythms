2249a1e891354a1e4c159850c7d8144906857cd8
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-21 12:36:03 -0400

    Time: 159 ms, Memory: 57.9 MB - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -9,27 +9,28 @@ var threeSum = function(nums) {
     nums.sort((a,b) => a-b)
 
     for (let i = 0; i < nums.length; i++) {
-        const a = nums[i];
-        if (a > 0) break;
-        if (i > 0 && a === nums[i - 1]) continue;
+        const a = nums[i]
+        if (a > 0) break
+        // not first val in input arr & same value as before.. don't wanna reuse
+        if (i > 0 && a === nums[i - 1]) continue
 
-        let l = i + 1;
-        let r = nums.length - 1;
-        while (l < r) {
-            const threeSum = a + nums[l] + nums[r];
-            if (threeSum > 0) {
-                r--;
-            } else if (threeSum < 0) {
-                l++;
+        let left = i + 1
+        let right = nums.length - 1
+        
+        while (left < right) {
+            const sum = a + nums[left] + nums[right]
+            
+            if (sum > 0) {
+                right--
+            } else if (sum < 0) {
+                left++
             } else {
-                res.push([a, nums[l], nums[r]]);
-                l++;
-                r--;
-                while (nums[l] === nums[l - 1] && l < r) {
-                    l++;
-                }
+                res.push([a, nums[left], nums[right]])
+                // update pointers
+                left++
+                while (nums[left] === nums[left - 1] && left < right) left++
             }
         }
     }
-    return res;
+    return res
 };
