bb54e4b47b067be759a902ab50240d0a83cceb7f
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-21 15:13:19 -0400

    Time: 67 ms, Memory: 44.4 MB - LeetHub

---
+++ string-compression/string-compression.js
@@ -0,0 +1,34 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+// T: O(n) - S: O(1)
+
+var compress = function(chars) {
+    let prevChar = chars[0]
+    let idx = 0
+    let len = 1
+    
+    for (let i = 1; i <= chars.length; i++) {
+        if (chars[i] === prevChar) {
+            len++
+            continue
+        }
+        
+        chars[idx] = prevChar
+        idx++
+        
+        if (len > 1) {
+            let lenArr = len.toString().split('')
+            for (let char of lenArr) {
+                chars[idx] = char
+                idx++
+            }
+        }
+        
+        prevChar = chars[i]
+        len = 1
+    }
+    
+    while (chars.length > idx) chars.pop()
+};
