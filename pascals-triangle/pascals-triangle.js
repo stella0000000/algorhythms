ebdfd0347c42c8760521ee81a653331a9a9900c4
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-22 12:08:26 -0400

    Time: 49 ms, Memory: 42.2 MB - LeetHub

---
+++ pascals-triangle/pascals-triangle.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let output = [[1]]
+    
+    for (let i=0; i<numRows-1; i++) {
+        let lastRow = output[output.length-1]
+        let row = [1]
+        
+        for (let j=1; j<lastRow.length; j++) {
+            row.push(lastRow[j] + lastRow[j-1])
+        }
+        
+        row.push(1)
+        output.push(row)
+        
+    }
+    
+    return output
+};
