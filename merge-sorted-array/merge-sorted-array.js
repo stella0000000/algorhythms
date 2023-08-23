33ee864a9a4109b2026f5977c995f523d25504b6
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-23 11:56:44 -0400

    Time: 49 ms, Memory: 42.3 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.js
+++ merge-sorted-array/merge-sorted-array.js
@@ -8,8 +8,7 @@
 var merge = function(nums1, m, nums2, n) {
     m--
     n--
-    let i = nums1.length - 1
-
+    
     for (let i=nums1.length-1; i>=0; i--) {
         if (n > -1 && m > -1 && nums1[m] >= nums2[n]) { // swap
             nums1[i] = nums1[m]
