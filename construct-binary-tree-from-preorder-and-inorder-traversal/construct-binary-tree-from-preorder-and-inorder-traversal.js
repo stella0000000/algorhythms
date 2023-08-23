b4c58298dc0db053c6cbd94fc4e0fa5a6702cd21
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-23 12:39:57 -0400

    Time: 111 ms, Memory: 137.9 MB - LeetHub

--- construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -26,7 +26,7 @@ const buildTree = (preorder, inorder) => {
     // preorder     root => left => right
     // inorder      left => root => right
     
-    root.left = buildTree(preorder.slice(1, mid+1), inorder.slice(0, mid))
+    root.left = buildTree(preorder.slice(1), inorder.slice(0, mid))
     root.right = buildTree(preorder.slice(mid+1), inorder.slice(mid+1))
     
     return root
