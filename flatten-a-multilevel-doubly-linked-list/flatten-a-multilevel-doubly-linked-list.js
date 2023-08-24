13b6c7e8acf96af37017b7478c7db33d8f354740
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-24 14:49:54 -0400

    Time: 58 ms, Memory: 43.7 MB - LeetHub

---
+++ flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.js
@@ -0,0 +1,46 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+// input: head of doubly linked list
+        // nodes: next, previous, child
+        // child may or may not be separate doubly linked
+            // may have 1+ children
+// output: multilevel data structure
+    // flattened
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function(head) {
+    // DFS => stack
+    if (!head) return null
+
+    let curr = head
+    const stack = []
+
+    while (curr !== null) {
+        if (curr.child) {
+            if (curr.next) stack.push(curr.next)  // reserve in stack
+            curr.next = curr.child
+            curr.next.prev = curr
+            curr.child = null  // taken care of
+        } else if (curr.next === null && stack.length) {
+            // no next, but nodes remain from previous iterations
+            curr.next = stack.pop()
+            curr.next.prev = curr
+        }
+
+        // increment curr
+        curr = curr.next
+    }
+
+    return head
+};
