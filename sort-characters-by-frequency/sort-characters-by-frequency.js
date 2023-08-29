693a364c201e00193e65f3d0fd7dae8e84f54d4f
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-08-29 14:25:46 -0400

    Time: 70 ms, Memory: 49.6 MB - LeetHub

---
+++ sort-characters-by-frequency/sort-characters-by-frequency.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    const hash = {}
+    const res = []
+    let str = ''
+    
+    for (let char of s) {
+        if (!(char in hash)) hash[char] = 0
+        hash[char]++
+    }
+    
+    for (let char in hash) {
+        res.push([char, hash[char]])
+    }
+    
+    res.sort((a, b) => b[1]-a[1])
+    
+    for (let ele of res) {
+        let [char, num] = ele
+        
+        str += repeat(char, num)
+        
+    }
+    
+    return str
+};
+
+const repeat = (char, num) => {
+    let res = ''
+    
+    for (let i=0; i<num; i++) res+=char
+    
+    return res
+}
