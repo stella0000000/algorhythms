33aab90023f498388d040b01fa060cc2bbcb974f
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-09-07 10:12:19 -0400

    Time: 46 ms (98.43%), Space: 42.4 MB (94.07%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let left = 0
+    let right = height.length -1
+    let lh = height[left]
+    let rh = height[right]
+    let water = 0
+
+    while (left < right) {
+        // fill from left
+        while (left < right && lh <= rh) {
+            left++
+            if (height[left] > lh) {
+                lh = height[left]
+            } else {
+                water += lh - height[left]
+            }
+        }
+        
+        // fill from right
+        while (left < right && lh >= rh) {
+            right--
+            if (height[right] > rh) {
+                rh = height[right]
+            } else {
+                water += rh - height[right]
+            }
+        }
+    }
+
+    return water
+};
+
+
+// # [0,1,0,2,1,0,1,3,2,1,2,1] = 14
+// # [0,1,1,2,2,2,2,3,2,2,2,1] = 20
+// # 6 water
+
+// # [0,1,0,2,1,0,1,3,2,1,2,1] hold one pointer at fill height element
+// #    ^ -> fill with 1
+// # [0,1,1,2,1,0,1,3,2,1,2,1] when we get to equal or greater height, update fill height and switch pointer
+// #        ^ -> fill with 2
+// # [0,1,1,2,2,2,2,3,3,3,3,3] never got to anything bigger or equal
+// #                ^ -> fill with 3
+// # [0,1,1,2,2,2,2,3,2,2,2,x] step ahead to next smaller element and try again
+// #                  ^   ^
+// # [0,1,1,2,2,2,2,3,2,2,2,2] never find anything bigger or equal
+// #                      ^
+// # [0,1,1,2,2,2,2,3,2,2,2,1] at end, finish
+// #                        ^
+
+// # [0,1,1,2,2,2,2,3,2,2,2,1] two pointers, alternate to whichever is strictly less than
