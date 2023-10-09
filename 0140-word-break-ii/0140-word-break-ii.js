b419cc924779500f445d9a9ed4a8507bbe8339d3
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-10-09 12:22:15 -0400

    Time: 43 ms (90.08%), Space: 42 MB (31.35%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.js
+++ 0140-word-break-ii/0140-word-break-ii.js
@@ -3,30 +3,75 @@
  * @param {string[]} wordDict
  * @return {string[]}
  */
-var wordBreak = function(s, wordDict) {
-    if (s == "") return []
-    let memo = {}
+
+// explore all paths in DFS + memoization
+    // splitting into smaller pieces
+    // same subproblem for substrings
+    // divide and conquer
+    // want to reach an empty string
+    // need to RETURN all the possible splits
+    // a list of all the words to create on split
+// memoization, exploring all paths through DFS, could be duplicates
+// parse from left to right
+// can we make a split
+
+
+const wordBreak = (s, wordDict) => {
+    const arr = []
     
-    const helper = (s) => {
-        if (s in memo) return memo[s] 
-        
-        let ans = []
-        for (let word of wordDict) {
-            if (!s.startsWith(word)) continue // base case
-            if (word.length == s.length) ans.push(word) // base case
-            
-            else {
-                let resultOfRest = helper(s.substr(word.length)) 
-                for (let v of resultOfRest) {
-                    v = word + ' ' + v
-                    ans.push(v)
-                }
-            }
+    const go = (str, val) => {
+        if (str === '') {
+            arr.push(val.trim())
+            return
         }
         
-        memo[s] = ans
-        return ans 
+        for (let word of wordDict) {
+            if (str.startsWith(word)) {
+                go(str.slice(word.length), val + word + ' ');
+            }
+        }
     }
     
-    return helper(s)
+    go(s, '')
+    return arr
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var wordBreak = function(s, wordDict) {
+//     if (s.length === 0) return []
+//     let memo = {}
+    
+//     const helper = (s) => {
+//         if (s in memo) return memo[s] 
+        
+//         let ans = []
+//         for (let word of wordDict) {
+//             if (!s.startsWith(word)) continue // base case
+//             if (word.length == s.length) ans.push(word) // base case
+            
+//             else {
+//                 let resultOfRest = helper(s.substr(word.length)) 
+//                 for (let v of resultOfRest) {
+//                     v = word + ' ' + v
+//                     ans.push(v)
+//                 }
+//             }
+//         }
+        
+//         memo[s] = ans
+//         return ans 
+//     }
+    
+//     return helper(s)
+// }
