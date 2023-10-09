c3f9fc856fa75615900dc5963b396fc595113d7e
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-10-09 11:28:00 -0400

    Time: 53 ms (99.48%), Space: 50.9 MB (6.22%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.js
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.js
@@ -17,7 +17,8 @@
  * @param {ListNode} head
  * @return {TreeNode}
  */
-var sortedListToBST = function(head) {
+
+const sortedListToBST = (head) => {
     // linked list to array
     // sort BST
     if (!head) return head
@@ -25,35 +26,75 @@ var sortedListToBST = function(head) {
     const sortedArr = convertToSortedArr(head)
     let mid = Math.floor(sortedArr.length / 2)
     
-    let newRoot = sortedArr[mid]
-    newRoot.left = balanceBSTarr(sortedArr.slice(0, mid))
-    newRoot.right = balanceBSTarr(sortedArr.slice(mid+1))
-
+    const newRoot = sortedArr[mid]
+    newRoot.left = balanceBST(sortedArr.slice(0, mid))
+    newRoot.right = balanceBST(sortedArr.slice(mid+1))
+          
     return newRoot
 }
 
-const balanceBSTarr = (arr) => {
+const balanceBST = (arr) => {
     if (arr.length === 0) return null
     if (arr.length === 1) return arr[0]
     
     let mid = Math.floor(arr.length / 2)
     
     let root = arr[mid]
-    root.left = balanceBSTarr(arr.slice(0, mid))
-    root.right = balanceBSTarr(arr.slice(mid+1))
-
+    root.left = balanceBST(arr.slice(0, mid))
+    root.right = balanceBST(arr.slice(mid+1))
+    
     return root
 }
 
-// sorted arr of LL nodes, but then make it into treenodes for our BST
 const convertToSortedArr = (head) => {
     let curr = head
-    let sorted = []
-    
+    const sorted = []
     while (curr) {
         sorted.push(new TreeNode(curr.val))
         curr = curr.next
     }
-    
     return sorted
 }
+
+
+
+// var sortedListToBST = function(head) {
+//     // linked list to array
+//     // sort BST
+//     if (!head) return head
+    
+//     const sortedArr = convertToSortedArr(head)
+//     let mid = Math.floor(sortedArr.length / 2)
+    
+//     let newRoot = sortedArr[mid]
+//     newRoot.left = balanceBSTarr(sortedArr.slice(0, mid))
+//     newRoot.right = balanceBSTarr(sortedArr.slice(mid+1))
+
+//     return newRoot
+// }
+
+// const balanceBSTarr = (arr) => {
+//     if (arr.length === 0) return null
+//     if (arr.length === 1) return arr[0]
+    
+//     let mid = Math.floor(arr.length / 2)
+    
+//     let root = arr[mid]
+//     root.left = balanceBSTarr(arr.slice(0, mid))
+//     root.right = balanceBSTarr(arr.slice(mid+1))
+
+//     return root
+// }
+
+// // sorted arr of LL nodes, but then make it into treenodes for our BST
+// const convertToSortedArr = (head) => {
+//     let curr = head
+//     let sorted = []
+    
+//     while (curr) {
+//         sorted.push(new TreeNode(curr.val))
+//         curr = curr.next
+//     }
+    
+//     return sorted
+// }
