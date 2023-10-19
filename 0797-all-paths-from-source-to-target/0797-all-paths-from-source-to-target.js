b67da6e17761f141549d5348f60ce0c5b63de18a
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-10-19 11:54:15 -0400

    Time: 91 ms (35.38%), Space: 52.1 MB (21.15%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -1,24 +1,64 @@
 const allPathsSourceTarget = (graph) => {
-  // directed acyclic graph
+    // global all paths arr to return
     const allPaths = []
+    
+    // dfs from source node 0
     findPaths(0, graph, allPaths, [])
+    
+    // returnz all the pathz
     return allPaths
 }
 
-const findPaths = (node, graph, allPaths, currPaths) => {
-    currPaths.push(node)
+const findPaths = (node, graph, allPaths, currPath) => {
+    // push the node in currPath[] we are pathing
+    currPath.push(node)
     
+    // if we are at final node
+    // i.e. node = n-1
+    // we can add currPath to all paths and return
+    // from this recursive call
     if (node === graph.length-1) {
-        allPaths.push([...currPaths])
+        allPaths.push(currPath)
         return
     }
     
-    for (let neighbor of graph[node]) {
-        findPaths(neighbor, graph, allPaths, [...currPaths])
+    // now we need to traverse neighbors from
+    // curr node
+    // clone currPath for diverging recursive calls
+    for (const neighbor of graph[node]) {
+        findPaths(neighbor, graph, allPaths, [...currPath])
     }
     
     return
 }
+
+
+
+
+
+
+
+// const allPathsSourceTarget = (graph) => {
+//   // directed acyclic graph
+//     const allPaths = []
+//     findPaths(0, graph, allPaths, [])
+//     return allPaths
+// }
+
+// const findPaths = (node, graph, allPaths, currPath) => {
+//     currPaths.push(node)
+    
+//     if (node === graph.length-1) {
+//         allPaths.push([...currPath])
+//         return
+//     }
+    
+//     for (let neighbor of graph[node]) {
+//         findPaths(neighbor, graph, allPaths, [...currPath])
+//     }
+    
+//     return
+// }
 
 
 
