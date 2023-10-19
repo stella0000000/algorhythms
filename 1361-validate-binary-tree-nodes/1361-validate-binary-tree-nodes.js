5d4846ff24f8120b9781960334c6b8d917aaf91a
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-10-19 12:55:54 -0400

    Time: 81 ms (45.13%), Space: 51.9 MB (37.66%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} n
+ * @param {number[]} leftChild
+ * @param {number[]} rightChild
+ * @return {boolean}
+ */
+var validateBinaryTreeNodes = function(n, leftChildArr, rightChildArr) {
+    const leftSet = new Set(leftChildArr)
+	const rightSet = new Set(rightChildArr)
+	let root = null
+    
+    let count = 0
+
+	// find root node
+	for (let i=0; i<n; i++) {
+		if (!leftSet.has(i) && !rightSet.has(i)) {
+			root = i
+            count++
+   		}
+        
+        if (count > 1) return false
+    }
+         2
+        /
+       3
+    0
+   /
+  1
+    
+    const visited = new Set()
+    const queue = [ root ]
+    while (queue.length) {
+        const node = queue.shift()
+        if (visited.has(node)) return false
+        visited.add(node)
+
+        let leftChild = leftChildArr[node]
+        let rightChild = rightChildArr[node]
+
+        if (leftChild !== -1) queue.push(leftChild)
+        if (rightChild !== -1) queue.push(rightChild)
+    }
+
+    return visited.size === n
+
+};
