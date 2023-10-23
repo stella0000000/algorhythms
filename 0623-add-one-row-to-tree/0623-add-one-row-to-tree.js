ebc41e7275d0964e577c73742f7265b33f8ade6a
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-10-22 22:33:58 -0400

    Time: 88 ms (8.24%), Space: 49.9 MB (5.88%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.js
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.js
@@ -14,39 +14,42 @@
  */
 
 const addOneRow = (root, val, depth) => {
+    // if depth is 1, create new root node
     if (depth === 1) return new TreeNode(val, root)
-    const stack = [[root, depth]]
+    
+    // stack starts @ root, depths to travel
+    const stack = [[ root, depth ]]
     
     while (stack.length) {
-        let [curNode, curDepth] = stack.pop()
-        curDepth--
+        let [node, numDepths] = stack.pop()
         
-        if (curDepth === 1) {
-            curNode.left = new TreeNode(val, curNode.left)
-            curNode.right = new TreeNode(val, null, curNode.right)
-        } else if (curDepth > 1) {
-            if (curNode.left) stack.push([curNode.left, curDepth])
-            if (curNode.right) stack.push([curNode.right, curDepth])
+        // for following levels
+        if (numDepths - 1 === 1) {
+            node.left = new TreeNode(val, node.left)
+            node.right = new TreeNode(val, null, node.right)
+        } else if (numDepths - 1 > 1) {
+            if (node.left) stack.push([ node.left, numDepths-1 ])
+            if (node.right) stack.push([ node.right, numDepths-1 ])
         }
     }
     
     return root
 }
 
-// var addOneRow = function(root, val, depth) {
-//     if (depth === 1) return new TreeNode(val, root)
+// // var addOneRow = function(root, val, depth) {
+// //     if (depth === 1) return new TreeNode(val, root)
 
-// 	const refactor = (node = root, currentDep = 1) => {
-// 		if (!node) return
-// 		if (currentDep === depth - 1) {
-// 			const { left, right } = node
-// 			node.left = new TreeNode(val, left)
-// 			node.right = new TreeNode(val, null, right)
-// 		}
-// 		refactor(node.left, currentDep + 1)
-// 		refactor(node.right, currentDep + 1)
-// 	}
+// // 	const refactor = (node = root, currentDep = 1) => {
+// // 		if (!node) return
+// // 		if (currentDep === depth - 1) {
+// // 			const { left, right } = node
+// // 			node.left = new TreeNode(val, left)
+// // 			node.right = new TreeNode(val, null, right)
+// // 		}
+// // 		refactor(node.left, currentDep + 1)
+// // 		refactor(node.right, currentDep + 1)
+// // 	}
     
-// 	refactor()
-// 	return root
-// }
+// // 	refactor()
+// // 	return root
+// // }
