6505fc55d00824e32d73dad8b202e5b7e88f3492
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-10-24 21:57:50 -0400

    Time: 59 ms (11.20%), Space: 41.6 MB (84.00%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.js
+++ 0140-word-break-ii/0140-word-break-ii.js
@@ -4,6 +4,23 @@
  * @return {string[]}
  */
 
+/*
+    input: s, wordDict
+    return: add spaces in s to construct a sentence w valid dict words
+        all possible sentences
+
+// explore all paths in DFS + memoization
+    // splitting into smaller pieces
+    // same subproblem for substrings
+    // divide and conquer
+    // want to reach an empty string
+    // need to RETURN all the possible splits
+    // a list of all the words to create on split
+// memoization, exploring all paths through DFS, could be duplicates
+// parse from left to right
+// can we make a split
+*/
+
 // explore all paths in DFS + memoization
     // splitting into smaller pieces
     // same subproblem for substrings
@@ -16,92 +33,35 @@
 // can we make a split
 
 const wordBreak = (s, wordDict) => {
-    if (s == "") return []
-    
-    // since we are employing DFS, we want to cache any duplicate subproblems
+    if (s === '') return []
     const memo = {}
     
-    const dfs = (s) => {
-        if (s in memo) return memo[s] 
+    const dfs = (currStr) => {
+        if (currStr in memo) return memo[currStr]
+        const res = []
         
-        let res = []
         for (let word of wordDict) {
-            // base cases
-            if (!s.startsWith(word)) continue
-            if (word.length == s.length) res.push(word)
+            if (!currStr.startsWith(word)) continue
+            // if (word.length === s.length) res.push(word)
             
-            else {
-                let resultOfRest = dfs(s.slice(word.length)) 
-                for (let v of resultOfRest) {
-                    v = word + ' ' + v
-                    res.push(v)
+            if (currStr.startsWith(word)) {
+                const newStr = currStr.slice(word.length)
+                const nextWords = dfs(newStr)
+                
+                if (!newStr.length && !nextWords.length) {
+                    res.push(word)
+                }
+                
+                for (let w of nextWords) {
+                    w = word + ' ' + w
+                    res.push(w)
                 }
             }
         }
         
-        memo[s] = res
+        memo[currStr] = res
         return res 
     }
     
     return dfs(s)
 }
-
-// const wordBreak = (s, wordDict) => {
-//     const arr = []
-    
-//     const go = (str, val) => {
-//         if (str === '') {
-//             arr.push(val.trim())
-//             return
-//         }
-        
-//         for (let word of wordDict) {
-//             if (str.startsWith(word)) {
-//                 go(str.slice(word.length), val + word + ' ');
-//             }
-//         }
-//     }
-    
-//     go(s, '')
-//     return arr
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var wordBreak = function(s, wordDict) {
-//     if (s.length === 0) return []
-//     let memo = {}
-    
-//     const helper = (s) => {
-//         if (s in memo) return memo[s] 
-        
-//         let ans = []
-//         for (let word of wordDict) {
-//             if (!s.startsWith(word)) continue // base case
-//             if (word.length == s.length) ans.push(word) // base case
-            
-//             else {
-//                 let resultOfRest = helper(s.substr(word.length)) 
-//                 for (let v of resultOfRest) {
-//                     v = word + ' ' + v
-//                     ans.push(v)
-//                 }
-//             }
-//         }
-        
-//         memo[s] = ans
-//         return ans 
-//     }
-    
-//     return helper(s)
-// }
