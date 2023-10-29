c718b2c91f111ba6262ba279c08f879268243956
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-10-28 23:30:12 -0400

    Time: 291 ms (5.56%), Space: 75.1 MB (6.67%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.js
+++ 1035-uncrossed-lines/1035-uncrossed-lines.js
@@ -3,24 +3,38 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var maxUncrossedLines = function(nums1, nums2) {
-    let n1 = nums1.length
-    let n2 = nums2.length
+
+/*
+    [1 3 2 4]
+     |  /  |     ... or \
+    [1 2 3 4]
+
+drawing lines connecting 2 numbers
+    nums1[i] to nums2[j]
+    such that they don't intersect any other connections
     
+    return max num of connecting lines we can draw in this way
+*/
+
+const maxUncrossedLines = (nums1, nums2) => {
+    const n1 = nums1.length
+    const n2 = nums2.length
     const memo = {}
+    // key is [i,j], val is # of lines
     
     const dfs = (i, j) => {
-        // we are building from the back
-        if (i<= 0 || j<=0) return 0
+        if (i <= 0 || j <= 0) return 0
         
         const key = i + ',' + j
         if (key in memo) return memo[key]
+        // key is [i,j], val is # of lines
         
-        // we can cross these lines
         if (nums1[i-1] === nums2[j-1]) {
-            memo[key] = 1 + dfs(i-1, j-1)
+            memo[key] = 1 + dfs(i-1, j-1) // backbuilding
         } else {
-            // we want the max lines crossed if the curr nums differ
+            // take max from other nums
+            // no +1 since at this stack call
+            // nums don't equal across arrays
             memo[key] = Math.max(dfs(i-1, j), dfs(i, j-1))
         }
         
@@ -28,12 +42,4 @@ var maxUncrossedLines = function(nums1, nums2) {
     }
     
     return dfs(n1, n2)
-};
-
-/*
-drawing lines connecting 2 numbers
-    nums1[i] to nums2[j]
-    such that they don't intersect any other connections
-    
-    return max num of connecting lines we can draw in this way
-*/
+}
