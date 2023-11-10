41804a1070280922408400f5cdac83afee9222dd
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-11-10 12:31:14 -0500

    Time: 66 ms (5.49%), Space: 41.7 MB (72.95%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -3,75 +3,141 @@
  * @return {string}
  */
 
-// k[encouded_string]
+// k[encoded_string]
 // 3[a2[c]] = accacc
 
-
-const decodeString = (s) => {
-    const nums = '0123456789'
-    let currstr = ''
-    let currnum = 0
-    const stack = []
+// const decodeString = (s) => {
+//     const stack = []
+//     let currNum = 0
+//     let currStr = ''
     
-    for (let char of s) {
+//     for (const char of s) {
+        
+//     }
+// }
+
+var decodeString = function(s) {
+    // return currStr
+    const stack = []
+    let currNum = 0
+    let currStr = ''
+
+    for (const char of s) {
         if (char === '[') {
-            stack.push(currnum)
-            stack.push(currstr)
-            currstr = ''
-            currnum = 0
+            stack.push([currStr, currNum])
+            currStr = ''
+            currNum = 0
         } else if (char === ']') {
-            prevstr = stack.pop()
-            num = stack.pop()
-            currstr = prevstr + repeat(num, currstr)
-        } else if (nums.includes(char)) {
-            currnum = 10 * currnum + Number(char)
-        } else {
-            currstr += char
+            //build
+            const [ prevStr, prevNum ] = stack.pop()
+            currStr = prevStr + currStr.repeat(prevNum)
+        } else if (!isNaN(char)) {    // string = Number
+            currNum = currNum * 10 + Number(char)
+        } else {                      // str
+            currStr += char
         }
     }
-    
-    return currstr
-}
+    return currStr
+};
 
+// const decodeString = (s) => {
+//     const stack = []
+//     const nums = '0123456789'
+
+//     for (const char of s) {
+//         if (nums.includes(char)) {
+//           stack.push(Number(char))
+//         } else if (char === ']') {
+//             let currStr = ''
+//             while (!nums.includes(stack[stack.length-1])) {
+//                 currStr = stack.pop() + currStr
+//             }
+
+//             let currNum = ''
+//             while (nums.includes(stack[stack.length-1])) {
+//                 currNum = stack.pop() + currNum
+//             }
+            
+//             stack.push(repeat(Number(currNum), currStr))
+//         } else if (char !== '[') {
+//             stack.push(char)
+//         }
+//     }
+
+//     return stack.join('')
+// }
+    
 const repeat = (num, str) => {
-    let message = ''
-    
-    for (let i=0; i<num; i++) {
-        message += str
-    }
-    return message
+  let res = ''
+  for (let i=0; i<num; i++) res += str
+  return res
 }
 
+// const decodeString = (s) => {
+//     const nums = '0123456789'
+//     let currstr = ''
+//     let currnum = 0
+//     const stack = []
+    
+//     for (let char of s) {
+//         if (char === '[') {
+//             stack.push(currnum)
+//             stack.push(currstr)
+//             currstr = ''
+//             currnum = 0
+//         } else if (char === ']') {
+//             let prevstr = stack.pop()
+//             let num = stack.pop()
+//             currstr = prevstr + repeat(num, currstr)
+//         } else if (nums.includes(char)) {
+//             currnum = 10 * currnum + Number(char)
+//         } else {
+//             currstr += char
+//         }
+//     }
+    
+//     return currstr
+// }
+
+// const repeat = (num, str) => {
+//     let message = ''
+    
+//     for (let i=0; i<num; i++) {
+//         message += str
+//     }
+//     return message
+// }
 
 
-// // tree of nested thing
 
-// // first parse input into logical structure holding "what is how many times"
-// // then build result
+// // // tree of nested thing
 
-// // const parseString = (s) => {
-// //     // what is the result structure here to holding logic
-// // }
+// // // first parse input into logical structure holding "what is how many times"
+// // // then build result
 
-// // var decodeString = function(s) {
-// //     // return currStr
-// //     const stack = []
-// //     let currNum = 0
-// //     let currStr = ''
+// // // const parseString = (s) => {
+// // //     // what is the result structure here to holding logic
+// // // }
 
-// //     for (const char of s) {
-// //         if (char === '[') {
-// //             stack.push([currStr, currNum])
-// //             currStr = ''
-// //             currNum = 0
-// //         } else if (char === ']') {
-// //             const [ prevStr, prevNum ] = stack.pop()
-// //             currStr = prevStr + currStr.repeat(prevNum)
-// //         } else if (!isNaN(char)) {    // string = Number
-// //             currNum = currNum * 10 + Number(char)
-// //         } else {                      // str
-// //             currStr += char
-// //         }
-// //     }
-// //     return currStr
-// // };
+// var decodeString = function(s) {
+//     // return currStr
+//     const stack = []
+//     let currNum = 0
+//     let currStr = ''
+
+//     for (const char of s) {
+//         if (char === '[') {
+//             stack.push([currStr, currNum])
+//             currStr = ''
+//             currNum = 0
+//         } else if (char === ']') {
+//             const [ prevStr, prevNum ] = stack.pop()
+//             currStr = prevStr + currStr.repeat(prevNum)
+//         } else if (!isNaN(char)) {    // string = Number
+//             currNum = currNum * 10 + Number(char)
+//         } else {                      // str
+//             currStr += char
+//         }
+//     }
+//     return currStr
+// };
