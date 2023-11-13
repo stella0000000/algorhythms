0bf1ca792a79920e5bce5e971b38fc8d0bcd0b84
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2023-11-12 20:03:34 -0500

    Time: 48 ms (93.90%), Space: 43.2 MB (79.36%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -4,6 +4,69 @@
  * @return {boolean}
  */
 
+// string, s
+// dictinary of strings, wordDict
+// true if s can be segmented with spaces
+// else false
+
+
+// Dynamic Programming
+const wordBreak = (s, wordDict) => {
+    const dp = new Array(s.length).fill(false)
+    // dp[i] means we can create a word from i-word.length to i
+    
+    for (let i=0; i<s.length; i++) {
+        for (const word of wordDict) {
+            // out of bounds
+            if (i < word.length-1) continue
+            
+            if (i === word.length-1 || dp[i-word.length]) {
+                let substr = s.slice(i-word.length+1, i+1)
+                
+                if (substr === word) {
+                    dp[i] = true
+                    break
+                }
+            }
+        }
+    }
+    console.log(dp)
+    return dp[s.length-1]
+}
+
+
+
+
+/*
+// BFS
+const wordBreak = (s, wordDict) => {
+    if (!s.length || !wordDict.length) return false
+    const dict = new Set(wordDict)
+    const q = [0]
+    const visited = new Set()
+    
+    while (q.length) {
+        const start = q.shift()
+        
+        if (!visited.has(start)) {
+            visited.add(start)
+            
+            for (let end=start+1; end<=s.length; end++) {
+                const substr = s.slice(start, end)
+
+                if (dict.has(substr)) {
+                    if (end === s.length) return true
+                    q.push(end)
+                }
+            }
+        }
+    }
+    
+    return false
+}
+*/
+
+
 // input string + object of words
 // return if the string can be formed with the words in dictionary
 
@@ -15,75 +78,22 @@
 //     // DYNAMIC PROGRAMMING
 //     if (!wordDict || wordDict.length === 0) return false
 //     const set = new Set(wordDict)
+    
 //     const dp = new Array(s.length+1).fill(false)
 //     dp[0] = true
     
 //     for (let end=1; end<=s.length; end++) {
 //         for (let start=0; start<end; start++) {
 //             const w = s.slice(start, end)
-//             // console.log({ start, end })
-//             if (dp[start] && set.has(w)) {
+            
+//             if (dp[start] === true && set.has(w)) {
 //                 dp[end] = true
 //                 break
 //             }
-            
-//             // console.log({ dp })
 //         }
-//         // console.log({ dp })
 //     }
     
 //     return dp[s.length]
-// }
-
-const wordBreak = (s, wordDict) => {
-    // BFS
-    if (!wordDict || wordDict.length === 0) return false
-    const set = new Set(wordDict)
-    
-    // When s = 'catsandog', wordDict = ['cats', 'ca', 'ts']
-  // After 'cats' and 'ca', it will become 'andog', 'tsandog'
-  // For 'tsandog', after 'ts', it will become 'andog' again, visited set here is for memoization
-    
-    const visited = new Set()
-    const queue = [0]
-    
-    while (queue.length) {
-        const start = queue.shift()
-        
-        // if we haven't visited this starting char
-        if (!visited.has(start)) {
-            for (let end=start+1; end <= s.length; end++) {
-                if (set.has(s.slice(start, end))) {
-                    if (end === s.length) return true
-                    queue.push(end)
-                }
-            }
-            visited.add(start)
-        }
-    }
-    
-    return false
-}
-
-
-// const wordBreak = (s, wordDict) => {
-//     let q = [0]
-//     let seen = {}
-
-//     for (const start of q) {
-//         if (start === s.length) return true
-
-//         for (const word of wordDict) {
-//             const end = start + word.length
-//             if (seen[end]) continue
-//             if (s.slice(start).startsWith(word)) {
-//                 seen[end] = true
-//                 q.push(end)
-//             }
-//         }
-//     }
-
-//     return false
 // }
 
 
