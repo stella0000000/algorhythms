2ea080e7855a195e3912245a1dd5292b691a4047
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2024-01-04 18:27:31 -0500

    Time: 59 ms (41.57%), Space: 44.4 MB (28.57%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -9,26 +9,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-const reverseList = (head) => {
-    // before: [PREV] [CURR] [CURR.NEXT]
-    // after:  [NEXT] [CURR] [PREV]
-    //                [PREV] [CURR] [NEXT]
-    
+var reverseList = function(head) {
     let prev = null
     let curr = head
     
     while (curr) {
-        // hold next in a temporary variable
         let next = curr.next
-        
-        // set next to (original) prev
         curr.next = prev
-        
-        // set prev to (original) next
-        prev = next
-        
-        prev = curr    // increment prev
-        curr = next    // increment curr
+        prev = curr
+        curr = next
     }
     
     return prev
