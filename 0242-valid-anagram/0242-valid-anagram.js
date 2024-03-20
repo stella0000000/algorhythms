323940291523f286abd7b5c698c42ac462e2fdf3
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2024-03-20 13:45:50 -0400

    Time: 73 ms (53.85%), Space: 54 MB (30.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,20 +3,24 @@
  * @param {string} t
  * @return {boolean}
  */
+
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
     
     const hash = {}
-
+    
     for (let letter of s) {
-        if (!hash[letter]) hash[letter] = 0
-        hash[letter]++
+        hash[letter] = (hash[letter] || 0) + 1
     }
-
+    
     for (let letter of t) {
         if (!hash[letter]) return false
         hash[letter]--
     }
-
-    return Object.values(hash).every(val => val === 0)
+    
+    for (const letter in hash) {
+        if (hash[letter] > 1) return false
+    }
+    
+    return true
 };
